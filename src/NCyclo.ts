095ed78cbfd05b29094aa0eef612fefe10b01b71
Jermaine Oliver <34130263+MadThink@users.noreply.github.com>
2021-10-16 17:35:30 -0400

    Main menu and sections added

--- src/NCyclo.ts
+++ src/NCyclo.ts
@@ -4,10 +4,45 @@ import { property } from 'lit/decorators.js';
 export class NCyclo extends LitElement {
   @property({ type: String }) title = 'My app';
 
-  static styles = css``;
+  static styles = css`
+  .main{
+    display:flex;
+    align-items: center;
+    height: 100vh;
+    background: white;
+  }
+  .selection-main{
+    display: flex;
+    padding: 10px;
+    border: 2px solid;
+    border-radius: 10px;
+    width: 80%;
+    margin: 0 auto;
+  }
+  .selection-item{
+    width: 300px;
+    height: 500px;
+    border: 2px solid;
+    border-radius: 10px;
+    padding: 20px;
+    margin: 20px;
+  }
+  `;
 
   render() {
     return html`
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" 
+    integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+    <div class="main">
+      <div class="selection-main">
+        <div class="selection-item selection-words">words</div>
+        <div class="selection-item selection-numbers">numbers</div>
+        <div class="selection-item selection-sudoku">sudoku</div>
+        <div class="selection-item selection-chess">chess</div>
+      </div>
+      </div>
     `;
   }
 }

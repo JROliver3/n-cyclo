ec8c4db679633505e05f4dbe8ffabbbfd333c270
Jermaine Oliver <34130263+MadThink@users.noreply.github.com>
2021-10-23 01:08:11 -0400

    loading clean stages and finalizing initial game mechanics for wordsmith

--- src/track/track.ts
+++ src/track/track.ts
@@ -1,6 +1,6 @@
-import { LitElement, html, css, property } from 'lit-element';
-import { navigator } from 'lit-element-router';
-
+import { LitElement, property } from 'lit-element';
+import { StageObject } from '../interfaces/stageInterface';
+import { Difficulty } from '../enums/game';
 /** 
  * Stage instances are occurrences of a stage (a collection of words with user attributes such as score, difficulty, etc.) 
  * that are independent of the stage types. 
@@ -15,78 +15,115 @@ import { navigator } from 'lit-element-router';
  * not be added to the track class but to the game class itself. 
 */
 
+
 export declare interface TrackStatus {
     // name of the track being run
     name: string;
 
 }
 
-declare interface StageMapObject {
-    name: string;
-    count: number;
-}
-
-
 export class Track extends LitElement {
-    @property({type: Array}) stages = [];
-    @property({type: Array}) stageBuffer: string[] = [];
-    @property({type: Array}) stageMap: StageMapObject[] = [];
-    @property({type: Number}) instanceCount = 0;
-    @property({type: Number}) round = 0;
+    @property({ type: Array }) stages: StageObject[] = [];
+    @property({ type: Array }) stageBuffer: string[] = [];
+    @property({ type: Number }) trackDifficulty: Difficulty = 0;
+    @property({ type: Number }) instanceCount = 0;
+    @property({ type: Number }) round = 0;
 
-    protected get stageInstance(){
-        if(this.stageBuffer.length == 0){
+    protected get stageInstance() {
+        if (this.stageBuffer.length == 0) {
             let instanceDistribution = this.getStageDistribution(10);
-            for(const stage of instanceDistribution){
-                for(var i=0; i<stage.count; i++){
+            for (const stage of instanceDistribution) {
+                for (var i = 0; i < stage.count; i++) {
                     this.stageBuffer.push(stage.name);
                     this.stageBuffer = this.shuffle(this.stageBuffer);
                 }
             }
         }
         let instance = this.stageBuffer.pop();
-        let stageMapInstance = this.stageMap.find((value)=>value.name === instance);
-        if(stageMapInstance){stageMapInstance.count++;}
-        return instance;
+        let stageInstance = this.stages.find((value) => value.name === instance);
+        if (stageInstance) { stageInstance.stageCount++; }
+        this.round++;
+        return stageInstance?.name;
+    }
+    // Provide full list of possible stages and let the track decide which to show based on the chosen track difficulty. A small subset of the 
+    // stages provided will actually be selected or used. Once all of the stages are used the track will end. The track is not meant to be completed
+    // for all of the stages provided, rather the user will make as much progress as possible with a timer and log their results. If the track ends then
+    // another book must be selected before proceeding. 
+    protected loadStages(bookId: string, allStages: string[], trackDifficulty = Difficulty.MEDIUM) {
+        let stageLimit = 0;
+        switch (trackDifficulty) {
+            case (Difficulty.EASY):
+                stageLimit = 5;
+                break;
+            case (Difficulty.MEDIUM):
+                stageLimit = 10;
+                break;
+            case (Difficulty.HARD):
+                stageLimit = 15;
+                break;
+            case (Difficulty.EXPERT):
+                stageLimit = 20;
+                break;
+            case (Difficulty.LEGEND):
+                stageLimit = 25;
+                break;
+            case (Difficulty.ULTIMATE):
+                stageLimit = 30;
+                break;
+        }
+        let shuffledStages = this.shuffle(allStages);
+        for (var i = 0; i < stageLimit; i++) {
+            let stageDescription = shuffledStages[i];
+            let stageName = bookId + stageDescription;
+            this.stages.push({ name: stageName, description: stageDescription, stageDifficulty: Difficulty.STARTER, stageCount: 0 });
+        }
     }
 
-    private getStageDistribution(num: number){
-        let weights = this.stageMap.map((stage) => 1/stage.count);
+    protected getStageDifficulty(stageName: string){
+        let stage = this.stages.find((stage) => stage.name == stageName);
+        return stage?.stageDifficulty;
+    }
+
+    protected getStageDescription(stageName: string){
+        let stage = this.stages.find((stage) => stage.name == stageName);
+        return stage?.description;
+    }
+
+    private getStageDistribution(num: number) {
+        let weights = this.stages.map((stage) => (1 / (stage.stageCount + 1)));
         let total_weight = this.sum(weights);
         let distribution = [];
-        for(const stage of this.stageMap){
-            let weight = 1/stage.count;
+        for (const stage of this.stages) {
+            let weight = 1 / (stage.stageCount + 1);
             let p = weight / total_weight;
             let d = Math.round(p * num);
-            distribution.push({name:stage.name, count: d});
+            distribution.push({ name: stage.name, count: d });
             num -= d;
             total_weight -= weight;
         }
         return distribution;
     }
 
-    private sum(array: Array<number>){
+    private sum(array: Array<number>) {
         let sum = 0;
-        for(const number of array){
+        for (const number of array) {
             sum += number;
         }
         return sum;
     }
 
-    //Unbiased Fisher-Yates (aka Knuth) shuffle algorithm
-    private shuffle(array: string[]) {
-        let currentIndex = array.length,  randomIndex;
-      
+    private shuffle(array: any[]) {
+        let currentIndex = array.length, randomIndex;
+
         while (currentIndex != 0) {
-      
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex--;
-      
-          [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
+
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
         }
-      
+
         return array;
-      }
+    }
 }
- 
--- src/wordsmith/wordsmith.ts
+++ src/wordsmith/wordsmith.ts
@@ -1,8 +1,8 @@
 import { LitElement, html, css, property } from 'lit-element';
 import { navigator } from 'lit-element-router';
-
+import {Book, getBooks} from '../books/books.js';
 import { Track } from '../track/track';
-
+import { Difficulty } from '../enums/game';
 /** 
 * The wordsmith component works by creating stage instances and using a probability
 * distribution to determine which stage type should appear in each stage instance. 
@@ -12,28 +12,126 @@ import { Track } from '../track/track';
 * to track user progression.
 */
 
+declare interface WordChallenge {
+    // the string value of the word
+    value: string;
+    // whether the word is visible in the challenge
+    visible: boolean;
+}
+
 export class Wordsmith extends Track {
-    @property({type: String}) book = '';
-    @property({type: Object}) currentStage = {};
+    @property({ type: Object }) book: Book = {
+        title: "",
+        text: ""
+    };
+    @property({type: Array}) books: Book[] = [];
+    @property({ type: String }) currentStage = "";
 
     static styles = css`
     .wordsmith-main {
         display: flex;
     }`;
 
-    private selectBook(bookId: String){
+    firstUpdated(){
+        this.addEventListener("keypress", (e:KeyboardEvent)=> {
+            if(e.key == "Enter"){
+                this.submitAnswer();
+            }
+        });
+        this.selectBook();
+        this.nextTrack();
+    }
+    private submitAnswer(){
+        //get all textareas and parse response
+        console.log("answer submitted");
+    }
+    private selectBook(bookId: String = '') {
+        this.books = getBooks();
+        //temp auto select
+        this.book = this.books[0];
+    }
+    private getStagesFromBook(book: Book){
+        let splitBook = book.text.split('.');
+        let stages = splitBook.filter((sentence) => sentence.split(' ').length > 3);
+        let cleanStages = stages.map((stage) => stage.replace(/\'+|(\n)+|^\s+|\s+$|\s{2,}/g, '').trim());
+        return cleanStages;
+    }
 
+    private nextTrack(round = 0){
+        if(!this.book){ 
+            console.log("No book selected." );
+            return null;
+        }
+        if(round == 0){
+            let stages = this.getStagesFromBook(this.book);
+            this.loadStages(this.book.title.toString(), stages);
+        }
+        this.currentStage = this.stageInstance?.toString() || "";
+    }
+
+    private get stageChallenge() {
+        let difficulty = this.getStageDifficulty(this.currentStage);
+        let hiddenWordPercentage = 0;
+        switch(difficulty){
+            case(Difficulty.EASY):
+                hiddenWordPercentage = 0.25;
+                break;
+            case(Difficulty.MEDIUM):
+                hiddenWordPercentage = 0.33;
+                break;
+            case(Difficulty.HARD):
+                hiddenWordPercentage = .5;
+                break;
+            case(Difficulty.EXPERT):
+                hiddenWordPercentage = .66;
+                break;
+            case(Difficulty.LEGEND):
+                hiddenWordPercentage = .75;
+                break;
+            case(Difficulty.ULTIMATE):
+                hiddenWordPercentage = .8;
+                break;
+        }
+        let stageDescription = this.getStageDescription(this.currentStage) || '';
+        let words = stageDescription.split(' ');
+        let hiddenWordCount = words.length * hiddenWordPercentage;
+        let wordChallenge = this.getWordChallenge(words, hiddenWordCount);
+        return wordChallenge;
+    }
+
+    private getWordChallenge(words: string[], n: number){
+        let wordChallenge: WordChallenge[] = [];
+        for(var i=0; i<words.length; i++){
+            wordChallenge.push({value: words[i], visible: true});
+        }
+        let randomIndex = 0;
+        while(n>0){
+            randomIndex = Math.floor(Math.random() * words.length);
+            wordChallenge[randomIndex].visible = false;
+            n--;
+        }
+        return wordChallenge;
+    }
+
+    private checkValue(event: KeyboardEvent) {
     }
 
     render() {
         return html`
             <div class="wordsmith-main">
                 <div class="wordsmith-text-area">
-                    ${this.stageInstance}
+                    ${this.stageChallenge.forEach((word) => {
+            if (word.visible) {
+                return word.value;
+            } else {
+                let wordSpace = new Array(word.value.length + 1).join(' ');
+                return html`<textarea class=hidden-word>${wordSpace}</textarea>`
+            }
+        })}
                 </div>
             </div>
         `;
     }
 }
- 
+
 customElements.define('wordsmith-game', Wordsmith);
